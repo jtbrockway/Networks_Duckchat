b14b57a46601e1b3e92f5e69e906a17684c85021
Jack Brockway <jtbrockway22@gmail.com>
2017-10-19 16:00:23 -0700

    More server work

--- server.c
+++ server.c
@@ -9,13 +9,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+char *SERVER_HOST_IP_ADDRESS;
+int SERVER_PORT;
+
 int main(int argc, char *argv[]){
 
 		//Parse input
 		if(argc != 3){
-				perror("Please enter: Host address and Host IP");
+				perror("Please enter: Host address and Host Port");
 
 				exit(EXIT_FAILURE);
 		}
+
+
 		return 0;
 }
