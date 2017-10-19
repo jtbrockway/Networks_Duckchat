a7651397c9729ba7037bc98debc57593f55b88af
Jack Brockway <jtbrockway22@gmail.com>
2017-10-19 14:17:39 -0700

    Began working on server.c

--- server.c
+++ server.c
@@ -1,3 +1,21 @@
-int main(){
+#include "duckchat.h"
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <strings.h>
+#include <arpa/inet.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+
+		//Parse input
+		if(argc != 3){
+				perror("Please enter: Host address and Host IP");
+
+				exit(EXIT_FAILURE);
+		}
 		return 0;
 }
